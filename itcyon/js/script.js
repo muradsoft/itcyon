b06df79da476d1109dc9e3ce6d9df6c8b02d9798
muradsoft <hasl118777@gmail.com>
2020-10-29 20:47:31 +0300

    first commit

---
+++ itcyon/js/script.js
@@ -0,0 +1,51 @@
+
+//slider
+$(document).ready(function () {
+
+	// $("[data-btn=work]").on('click', function (e) {
+	// 	$('.add-block').toggleClass('add-block__active');
+	// 	$('[data-btn=work]').toggleClass('btn-addgreen_close')
+	// });
+
+	// $("[data-btn=worker]").on('click', function (e) {
+	// 	$('.chooseworker').toggleClass('chooseworker__active');
+	// 	$('[data-btn=worker]').toggleClass('btn-addgreen_close')
+	// });
+
+	//catalog descr
+
+	function toggleWork(item) {
+		$(item).each(function (i) {
+			$(this).on("click", function (e) {
+				e.preventDefault();
+				$('.add-block').eq(i).toggleClass('add-block__active');
+				$('[data-btn=work]').eq(i).toggleClass('btn-addgreen_close')
+			});
+		});
+	};
+
+	function toggleWorkers(item) {
+		$(item).each(function (i) {
+			$(this).on("click", function (e) {
+				e.preventDefault();
+				$('.chooseworker').eq(i).toggleClass('chooseworker__active');
+				$('[data-btn=worker]').eq(i).toggleClass('btn-addgreen_close')
+			});
+		});
+	};
+	toggleWork('[data-btn=work]');
+	toggleWorkers('[data-btn=worker]');
+	// 		});
+	// 	});
+	// };
+	// toggleSlide(".catalog-item__link");
+	// toggleSlide(".catalog-item__back");
+
+	// function modalClose(item) {
+	// 	$(item).on('click', () => {
+	// 		$('.overlay, #consultation, #order, #thanks').fadeOut();
+	// 	});
+	// };
+	// modalClose('.modal__close');
+
+});
