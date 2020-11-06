7db54428505c3f4133f95fdc66210fabeba5bc91
muradsoft <hasl118777@gmail.com>
2020-11-06 19:26:17 +0300

    last version

--- itcyon/js/script.js
+++ itcyon/js/script.js
@@ -46,7 +46,49 @@ $(document).ready(function () {
 			.closest('div.tasks__inner').find('div.tasks__content').removeClass('tasks__content_active').eq($(this).index()).addClass('tasks__content_active');
 	});
 
+
+
+	function tasksReady(item) {
+		$(item).each(function (i) {
+			$(this).on("click", function (e) {
+				e.preventDefault();
+				$('.tasks-block').eq(i).css('display', 'none');
+				let a = $('#span_tasks').text();
+				let b = $('#span_ready').text();
+				problem = $(this).text();
+				if (problem == 'Отмена') {
+					a++;
+					b--;
+				}
+				else {
+					a--;
+					b++;
+				}
+				$('#span_tasks').text(a);
+				$('#span_ready').text(b);
+			});
+		});
+	};
+
+	$('#regform_add').submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: 'POST',
+			url: '',
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find('input').val("");
+
+			$('#regform_add').trigger('reset');
+		})
+		return false;
+	});
+
+
+
 	toggleWork('[data-btn=work]');
 	toggleWorkers('[data-btn=worker]');
 	selectForm('#select_cooperator');
+	tasksReady('[data-btn=ready]')
+	// tasksClose('[data-btn=close]')
 });
